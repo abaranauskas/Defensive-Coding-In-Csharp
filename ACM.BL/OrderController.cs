e30e288726ab30ac761f8a4c467068186c0b0df7
Aidas <aidas.baranauskas@yahoo.com>
2018-09-07 17:24:56 +0300

    Add project files.

---
+++ ACM.BL/OrderController.cs
@@ -0,0 +1,46 @@
+﻿using Core.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class OrderController
+    {
+        public OrderController()
+        {
+            customerRepository = new CustomerRepository();
+            orderRepository = new OrderRepository();
+            inventoryRepository = new InventoryRepository();
+            emailLibrary = new EmailLibrary();
+        }
+
+        public void PlaceOrder(Customer customer,
+                               Order order,                               
+                               Payment payment,
+                               bool allowSplitOrders,
+                               bool receiptRequest)
+        {            
+            customerRepository.Add(customer);            
+            orderRepository.Add(order);           
+            inventoryRepository.OrderItems(order, allowSplitOrders);
+
+            payment.ProcessPayment();
+
+            if (receiptRequest)
+            {
+                customer.ValidateEmail();
+                customerRepository.Update();
+                
+                emailLibrary.SendEmail(customer.EmailAddress, "Cia yra Receipt");
+            }
+        }
+
+        private CustomerRepository customerRepository { get; set; }
+        private OrderRepository orderRepository { get; set; }
+        private InventoryRepository inventoryRepository { get; set; }
+        private EmailLibrary emailLibrary { get; set; }
+    }
+}
---
+++ ACM.Win/OrderWin.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ACM.BL;
+using Core.Common;
+
+namespace ACM.Win
+{
+    public partial class OrderWin : Form
+    {       
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            PlaceOrder();
+
+        }
+
+        private void PlaceOrder()
+        {
+            var customer = new Customer()
+            {   //cia turetu but ivedama per UI
+                CustomerId = 1,
+                FirtName = "Adas",
+                LastName = "Baranauskas",
+                EmailAddress = "baranauskas.aidas@gmail.com"
+            };
+
+            var order = new Order();
+            //pupilate order instance// cia per UI 
+
+            //var allowSplitOrders = true;
+            //per UI
+
+            var payment = new Payment();
+            //per UI
+
+            //var receiptRequest = true;
+            // per UI
+
+            var orderController = new OrderController();
+            //orderController.PlaceOrder(customer, order, payment, allowSplitOrders, receiptRequest);
+            orderController.PlaceOrder(customer, order, payment, 
+                allowSplitOrders:true,
+                receiptRequest:false);
+            //pagerina kodo skaityma
+        }
+
+    }
+}
